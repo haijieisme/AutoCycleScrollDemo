27b574ca401e47a6dceefb3dfd0d8f7fdea999a2
haiBear <876617048@qq.com>
2016-02-25 18:23:19 +0800

    1

--- AutoCycleScrollDemo/ViewController.m
+++ AutoCycleScrollDemo/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    aaaa
+    let v = UIView()
 }
 
 - (void)didReceiveMemoryWarning {
